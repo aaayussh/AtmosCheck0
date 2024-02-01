267b6fc1e9eab24ce55b234c57a49cb915ca5706
Aayush Chaudhari <aayushchaudhari1@gmail.com>
2024-02-02 02:01:42 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+   method: "GET",
+   headers: {
+      "X-RapidAPI-Key": "21947f5a0dmsh76e86521205022dp1ff093jsnff10ed5cc4d5",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+   },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "ea71d17183446a7e151f966db3e151a4"; 
